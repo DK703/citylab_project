f3e721c034156e9e41c40944841070d16fa516f3
DK703 <dkim3157@gmail.com>
2026-06-17 21:33:39 +0000

    fix rviz issue

--- robot_patrol/launch/start_patrolling.launch.py
+++ robot_patrol/launch/start_patrolling.launch.py
@@ -1,7 +1,16 @@
+import os
+from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch_ros.actions import Node
 
 def generate_launch_description():
+    
+
+    rviz_config = os.path.join(
+        get_package_share_directory('robot_patrol'),
+        'rivz',
+        'virtualrvizconfig.rviz'  # replace with your actual filename
+    )
     return LaunchDescription([
         Node(
             package='robot_patrol',
@@ -10,7 +19,7 @@ def generate_launch_description():
         Node(
             package='rviz2',
             executable='rviz2',
-            arguments=['-d', '/path/to/virtualrvizconfig.rviz'],
+            arguments=['-d', rviz_config],
             output='screen'
         )
     ])
